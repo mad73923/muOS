564d03055533aa8b51600e8288615cf37f117448
Matthias Weis <matthias1.weis@web.de>
2017-04-19 13:50:53 +0200

    header fix

--- muOS/inc/hardware.h
+++ muOS/inc/hardware.h
@@ -1,14 +1,18 @@
 /*
  * hardware.h
  *
- *  Created on: 19.04.2017
+ *  Created on: 15.04.2017
  *      Author: matthias
  */
 
-#ifndef PORTS_ARM_CM4_STM32L476RG_INC_HARDWARE_H_
-#define PORTS_ARM_CM4_STM32L476RG_INC_HARDWARE_H_
+#ifndef MUOS_INC_HARDWARE_H_
+#define MUOS_INC_HARDWARE_H_
 
+/**
+ * This function must be implemented by user.
+ * It should set up the hardware (System clock, Flash Latency, etc)
+ */
 void muOS_hardware_setup(void);
 void muOS_hardware_int_systick_enable(void);
 
-#endif /* PORTS_ARM_CM4_STM32L476RG_INC_HARDWARE_H_ */
+#endif /* MUOS_INC_HARDWARE_H_ */
