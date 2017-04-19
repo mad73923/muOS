588318f54337201f14dc2c4abf2d2db49db9a4db
Matthias Weis <matthias1.weis@web.de>
2017-04-19 12:41:26 +0200

    progress

--- muOS/inc/core.h
+++ muOS/inc/core.h
@@ -10,8 +10,8 @@
 
 #include "stm32l4xx.h"
 
-static inline void muOS_dispatcher(void);
-static inline uint32_t muOS_criticalSection_enter(void);
-static inline void muOS_criticalSection_leave(uint32_t status);
+__STATIC_INLINE void muOS_dispatcher(void);
+__STATIC_INLINE uint32_t muOS_criticalSection_enter(void);
+__STATIC_INLINE void muOS_criticalSection_leave(uint32_t status);
 
 #endif /* MUOS_INC_CORE_H_ */
--- ports/ARM_CM4_STM32L476RG/src/core.c
+++ ports/ARM_CM4_STM32L476RG/src/core.c
@@ -7,26 +7,26 @@
 
 #include "core.h"
 
-void __attribute__ ((naked)) SysTick_Handler(void){
+__attribute__ ((naked)) void SysTick_Handler(void){
 	asm("NOP");
-	//muOS_dispatcher();
+	muOS_dispatcher();
 }
 
 
-static inline void muOS_dispatcher(void){
+__attribute__( ( always_inline ) ) __STATIC_INLINE void muOS_dispatcher(void){
 	uint32_t irq_status = muOS_criticalSection_enter();
 
 	muOS_criticalSection_leave(irq_status);
 
 }
 
-static inline uint32_t muOS_criticalSection_enter(void){
+__attribute__( ( always_inline ) ) __STATIC_INLINE uint32_t muOS_criticalSection_enter(void){
 	uint32_t status = __get_PRIMASK();
 	__disable_irq();
 	return status;
 }
 
-static inline void muOS_criticalSection_leave(uint32_t status){
+__attribute__( ( always_inline ) ) __STATIC_INLINE void muOS_criticalSection_leave(uint32_t status){
 	if(status)
 		__enable_irq();
 }
